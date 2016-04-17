ce662da80d93501483c5a7653af0f59a6b61deb5
Adailson Filho <adailsonfilho@gmail.com>
2016-04-17 19:22:54 -0300

    booleans para habilitar/desabilitar stopwords e stemming

--- src/util/StemAnalyzer.java
+++ src/util/StemAnalyzer.java
@@ -9,14 +9,31 @@ import org.apache.lucene.analysis.core.StopFilter;
 import org.apache.lucene.analysis.en.PorterStemFilter;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.analysis.standard.StandardTokenizer;
+import org.apache.lucene.analysis.util.CharArraySet;
 
 public class StemAnalyzer extends Analyzer {
 
 	@Override
 	protected TokenStreamComponents createComponents(String fieldName) {
 
-		Tokenizer source = new StandardTokenizer(TokenStream.DEFAULT_TOKEN_ATTRIBUTE_FACTORY);		
-		return new TokenStreamComponents(source, new PorterStemFilter(new LowerCaseFilter(new StopFilter(source, StandardAnalyzer.STOP_WORDS_SET))));
+		Tokenizer source = new StandardTokenizer(TokenStream.DEFAULT_TOKEN_ATTRIBUTE_FACTORY);
+		
+		boolean stop = false;
+		boolean stem = false;
+		
+		if(stop){
+			if(stem){
+				return new TokenStreamComponents(source, new PorterStemFilter(new LowerCaseFilter(new StopFilter(source, StandardAnalyzer.STOP_WORDS_SET))));
+			}else{
+				return new TokenStreamComponents(source, new StopFilter(source, StandardAnalyzer.STOP_WORDS_SET));
+			}			
+		}else{
+			if(stem){
+				return new TokenStreamComponents(source, new PorterStemFilter(new LowerCaseFilter(new StopFilter(source, CharArraySet.EMPTY_SET))));				
+			}else{
+				return new TokenStreamComponents(source, new StopFilter(source, CharArraySet.EMPTY_SET));
+			}
+		}
 
 	}
 
